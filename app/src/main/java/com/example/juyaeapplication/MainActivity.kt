6feffb576b009e0a0a6f381900b82125db729749
JooYae <jooyae3970@naver.com>
2021-03-19 18:41:59 +0900

    set "lazy by"

--- app/src/main/java/com/example/juyaeapplication/MainActivity.kt
+++ app/src/main/java/com/example/juyaeapplication/MainActivity.kt
@@ -17,22 +17,51 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val fragmentHome by lazy { HomeFragment() }
+    private val fragmentMenu by lazy { MenuFragment() }
+    private val fragmentSearch by lazy { SearchFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initNavigation()
         initBinding()
+
     }
 
     private fun initNavigation() {
         NavigationUI.setupWithNavController(binding.navi, findNavController(R.id.navi_host))
+        binding.navi.run {
+            setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.nav_left -> {
+                        changeFragment(fragmentMenu)
+                    }
+
+                    R.id.nav_mid -> {
+                        changeFragment(fragmentHome)
+                    }
+                    R.id.nav_right -> {
+                        changeFragment(fragmentSearch)
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.nav_left
+        }
     }
 
-    private fun initBinding() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-    }
+
+        private fun initBinding() {
+            binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+            binding.lifecycleOwner = this
+        }
+
+        private fun changeFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                .commit()
+        }
+
 
 
 }
