6305621472bfd6fc1b38aed3f84efd683f69807a
JooYae <jooyae3970@naver.com>
2021-03-24 17:50:51 +0900

    custom selector

--- app/src/main/java/com/example/juyaeapplication/MainActivity.kt
+++ app/src/main/java/com/example/juyaeapplication/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.juyaeapplication.databinding.ActivityMainBinding
 import com.example.juyaeapplication.fragment.HomeFragment
 import com.example.juyaeapplication.fragment.MenuFragment
@@ -15,53 +17,19 @@ import com.example.juyaeapplication.fragment.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private val fragmentHome by lazy { HomeFragment() }
-    private val fragmentMenu by lazy { MenuFragment() }
-    private val fragmentSearch by lazy { SearchFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initNavigation()
-        initBinding()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initNavigation()
     }
 
     private fun initNavigation() {
-        NavigationUI.setupWithNavController(binding.navi, findNavController(R.id.navi_host))
-        binding.navi.run {
-            setOnNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.nav_left -> {
-                        changeFragment(fragmentMenu)
-                    }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navi_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navi.setupWithNavController(navController)
 
-                    R.id.nav_mid -> {
-                        changeFragment(fragmentHome)
-                    }
-                    R.id.nav_right -> {
-                        changeFragment(fragmentSearch)
-                    }
-                }
-                true
-            }
-            selectedItemId = R.id.nav_left
-        }
     }
-
-
-        private fun initBinding() {
-            binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-            binding.lifecycleOwner = this
-        }
-
-        private fun changeFragment(fragment: Fragment) {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
-                .commit()
-        }
-
-
-
 }
