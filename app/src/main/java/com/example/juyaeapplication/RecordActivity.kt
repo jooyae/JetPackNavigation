cd8ebf58e57c3ce89a8a6102ad1615d02c178af1
JooYae <jooyae3970@naver.com>
2021-04-12 23:51:22 +0900

    feat : reivew activity query

--- app/src/main/java/com/example/juyaeapplication/RecordActivity.kt
+++ app/src/main/java/com/example/juyaeapplication/RecordActivity.kt
@@ -1,11 +1,28 @@
 package com.example.juyaeapplication
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.OpenableColumns
 
 class RecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+        
+    }
+
+    @SuppressLint("Recycle")
+    private fun queryName(context: Context, uri: Uri): String {
+        val returnCursor: Cursor =
+            context.contentResolver.query(uri, null, null, null, null)!!
+        val nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        returnCursor.moveToFirst()
+        val name = returnCursor.getString(nameIndex)
+        returnCursor.close()
+        return name
     }
 }
